096bb9172935acc4540638c6ca906fddcc2ccd95
toonflash <toonflash@hotmail.com>
2018-10-02 02:38:38 +0000

    Implement downloadImageByURL added

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var githubToken = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -25,9 +26,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
         
     });
 
-  }
+}
+
+function downloadImageByURL(url, filePath) {
+
+    request.get(url)
+
+        .on('error', function (err) {
+            throw err;
+        })
+
+        .on('response', function (response) { 
+            console.log('Response Status Code: ', response.statusCode);
+        })
+
+       .pipe(fs.createWriteStream(filePath));
+
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-    //console.log("Errors:", err);
-    //console.log("Result:", result);
+    // console.log("Errors:", err);
+    // console.log("Result:", result);
 });
