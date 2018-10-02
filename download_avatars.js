28fb2f537e40facdbb12fb45bf49c851eb71b6e8
toonflash <toonflash@hotmail.com>
2018-10-02 00:56:21 +0000

    Adding Headers added

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,23 @@
 var request = require('request');
+var request = require('./secrets');
 
-//console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-}
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token GITHUB_TOKEN'
+        }
+    };
+
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+
+  }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
