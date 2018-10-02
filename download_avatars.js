f62a57af34e8bb79dc9608bd8db181796b1858dc
toonflash <toonflash@hotmail.com>
2018-10-02 03:39:32 +0000

    final commit before submiting

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,16 @@
 var request = require('request');
 var fs = require('fs');
 var githubToken = require('./secrets');
+// my two inputs from terminal
 var input1 = process.argv[2];
 var input2 = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+    // handles the error of someone not inputting repo and owner into terminal
     if (input1 == undefined || input2 == undefined) {
-        console.log("Please make sure you've entered a repo and owner name.");
+        console.log("Hmmm... It looks like you did not enter an owner or repo to search." + '\n' + "Please try again.");
         return false;
     }
 
@@ -22,9 +23,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
     };
 
     request(options, function(err, res, body) {
-        
         var parsedData = JSON.parse(body);
         parsedData.forEach(log)
+        
         cb(err, parsedData);
 
         function log(element) {
@@ -53,7 +54,4 @@ function downloadImageByURL(url, filePath) {
 
 }
 
-getRepoContributors(input1, input2, function(err, result) {
-    // console.log("Errors:", err);
-    // console.log("Result:", result);
-});
+getRepoContributors(input1, input2, function(err, result) {});
